d7d23a23d26750895d53dff32820ddc260d4740d
Seth Mcbay <sethmcbay@yahoo.com>
2019-03-04 15:11:06 -0500

    initial commit

---
+++ db/connection.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const mongoose = require(mongoose)
+
+// Connect to database
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+ } else {
+    mongoose.connect('mongodb://localhost/favsong');
+ }
+ mongoose.connection.on('error', function(err) {
+    console.error('MongoDB connection error: ' + err);
+    process.exit(-1);
+ });
+ mongoose.connection.once('open', function() {
+    console.log("Mongoose has connected to MongoDB!");
+ });
+ 
+ //export to mongoose
+ module.exports = mongoose
